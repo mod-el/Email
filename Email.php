6b947101a04c1b529ee258f51d74fb1c22747c4b
Domenico Giambra <sviluppo@editt.it>
2018-01-20 22:22:39 +0100

    Type hints and code reformatting

--- Email.php
+++ Email.php
@@ -5,12 +5,14 @@ use PHPMailer\PHPMailer\POP3;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-class Email{
+class Email
+{
 	protected $options;
 	protected $messaggio;
 	protected $main_address_set;
 
-	public function __construct($subject, $text, array $opt = []){
+	public function __construct(string $subject, string $text, array $opt = [])
+	{
 		$this->options = array_merge([
 			'from_name' => defined('EMAILS_DEFAULT_NAME') ? EMAILS_DEFAULT_NAME : '',
 			'from_mail' => defined('EMAILS_DEFAULT_SENDER') ? EMAILS_DEFAULT_SENDER : '',
@@ -26,13 +28,13 @@ class Email{
 		], $opt);
 		$this->main_address_set = false;
 
-		require_once(__DIR__.DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR.'phpmailer'.DIRECTORY_SEPARATOR.'PHPMailer.php');
-		require_once(__DIR__.DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR.'phpmailer'.DIRECTORY_SEPARATOR.'POP3.php');
-		require_once(__DIR__.DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR.'phpmailer'.DIRECTORY_SEPARATOR.'SMTP.php');
-		require_once(__DIR__.DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR.'phpmailer'.DIRECTORY_SEPARATOR.'Exception.php');
+		require_once(__DIR__ . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . 'phpmailer' . DIRECTORY_SEPARATOR . 'PHPMailer.php');
+		require_once(__DIR__ . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . 'phpmailer' . DIRECTORY_SEPARATOR . 'POP3.php');
+		require_once(__DIR__ . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . 'phpmailer' . DIRECTORY_SEPARATOR . 'SMTP.php');
+		require_once(__DIR__ . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . 'phpmailer' . DIRECTORY_SEPARATOR . 'Exception.php');
 
 		$this->messaggio = new PHPMailer();
-		if($this->options['smtp']) {
+		if ($this->options['smtp']) {
 			$this->messaggio->IsSMTP();
 			$this->messaggio->SMTPOptions = [
 				'ssl' => [
@@ -41,18 +43,18 @@ class Email{
 					'allow_self_signed' => true,
 				]
 			];
-			if($this->options['encryption'])
+			if ($this->options['encryption'])
 				$this->messaggio->SMTPSecure = $this->options['encryption'];
 			$this->messaggio->Host = $this->options['smtp'];
 			$this->messaggio->Port = $this->options['port'];
-			if($this->options['username']){
+			if ($this->options['username']) {
 				$this->messaggio->SMTPAuth = true;
 				$this->messaggio->Username = $this->options['username'];
-				if($this->options['password'])
+				if ($this->options['password'])
 					$this->messaggio->Password = $this->options['password'];
 			}
-			if(DEBUG_MODE and $this->options['debug'])
-				$this->messaggio->SMTPDebug  = 2;
+			if (DEBUG_MODE and $this->options['debug'])
+				$this->messaggio->SMTPDebug = 2;
 		}
 
 		$this->messaggio->IsHTML(true);
@@ -65,48 +67,53 @@ class Email{
 		$this->setText($text);
 	}
 
-	public function __destruct(){
+	public function __destruct()
+	{
 		unset($this->messaggio);
 	}
 
-	public function addAddress($send_to){
-		if(!is_array($send_to))
+	public function addAddress($send_to)
+	{
+		if (!is_array($send_to))
 			$send_to = [$send_to];
 
-		foreach($send_to as $c_em => $em){
-			if(!$this->main_address_set){
+		foreach ($send_to as $c_em => $em) {
+			if (!$this->main_address_set) {
 				$this->messaggio->AddAddress($em);
 				$this->main_address_set = true;
-			}else{
+			} else {
 				$this->messaggio->AddBCC($em);
 			}
 		}
 	}
 
-	public function setText($text){
-		$text = $this->options['header'].$text.$this->options['footer'];
+	public function setText(string $text)
+	{
+		$text = $this->options['header'] . $text . $this->options['footer'];
 		$this->messaggio->Body = $text;
 		$plain = html_entity_decode(trim(strip_tags(preg_replace('/<(head|title|style|script)[^>]*>.*?<\/\\1>/si', '', $text))), ENT_QUOTES, 'utf-8');
 		$this->messaggio->AltBody = $plain;
 	}
 
-	public function send($emails = null){
-		if($emails!==null)
+	public function send($emails = null): bool
+	{
+		if ($emails !== null)
 			$this->addAddress($emails);
 
-		if(!$this->messaggio->Send()){
+		if (!$this->messaggio->Send()) {
 			$sent = false;
-			if(DEBUG_MODE)
+			if (DEBUG_MODE)
 				echo $this->messaggio->ErrorInfo;
-		}else{
+		} else {
 			$sent = true;
 		}
-		if($this->options['smtp'])
+		if ($this->options['smtp'])
 			$this->messaggio->SmtpClose();
 		return $sent;
 	}
 
-	public function allegaFile($path, $name){
+	public function attachFile(string $path, string $name)
+	{
 		$this->messaggio->AddAttachment($path, $name);
 	}
 }
